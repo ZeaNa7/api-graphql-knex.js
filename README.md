dcd5418812d32461ddfaa78b68c20fb44f2918c3
Ana <niquita3@live.com>
2023-06-23 11:46:19 +0200

    ✨ (api): add home and readme

---
+++ README.md
@@ -0,0 +1,20 @@
+## Description
+
+This is an Graphql API built with Node.js and Express.js that uses the ORM Knex.js to interact with a MariaDB database (local Docker).
+
+## Installation
+
+1. Clone this repo
+2. Run `npm install`
+3. Create a `.env` file in the root directory and add the following variables:
+    - `DB_HOST`
+    - `DB_USER`
+    - `DB_PASSWORD`
+    - `DB_NAME` 
+    - `DB_PORT`
+4. Run `npm start` to start the server
+
+## Usage
+
+This API is used to interact with a MariaDB database. It can be used to create, read, update, and delete data from the database.
+Use localhost:3000/graphql as the base URL for all requests.
--- app.js
+++ app.js
@@ -3,11 +3,18 @@ import { importSchema } from "graphql-import";
 import { makeExecutableSchema } from "graphql-tools";
 import { graphqlHTTP } from "express-graphql";
 import resolvers from "./resolvers.js";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import path from 'path';
 
 const app = express()
 const port = 3000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.set('view engine', 'ejs');
+app.set('views', join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => {
     res.render('home');
